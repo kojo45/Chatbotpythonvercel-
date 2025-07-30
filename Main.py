1abdfbcb7b9bb7a53e9f246c26baa79c87d70fcf
kojo45 <kojsoja@gmail.com>
2025-07-30 13:46:47 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
 
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.post("/api/chatbot")
+async def chat(request: Request):
+    data = await request.json()
+    msg = data.get("message", "")
+    return JSONResponse(content={"response": f"Vous avez dit: {msg}"})
